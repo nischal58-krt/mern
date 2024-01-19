d7cf21ba12bd63f4a918755eea2fd2f5cbcb84df
nischal58-krt <156801092+nischal58-krt@users.noreply.github.com>
2024-01-19 15:00:04 +0545

    Updated

--- function.js
+++ function.js
@@ -15,7 +15,8 @@ function myFunction(msg) {
     //parameter -> inout
     console.log(msg);
 }
-myFunction("JS is intresting");// argument
+myFunction("JS is intresting")// argument
+
 function sum(a, b) {
     // local variables -> scope
     s = a + b;
@@ -59,3 +60,43 @@ function countVowels(str) {
     }
     console.log(count);
 }
+
+
+
+/*
+Higher Order Function/Methods 
+- either takes another function as parameter
+- or return another function as output
+- eg: arr.forEach
+syntax:
+arr.forEach((callBackFunctioin){
+CallbackFunction:Here it is a function to execute 
+for each element in the array
+-A call back is a function passed as an argument to 
+another function
+})
+*/ 
+let arr = ["Pokhara","Kathmandu","Butwal"];
+arr.forEach((val,idx,arr) =>{
+console.log ("Cities",(val),idx,arr);
+})
+
+/*for a given array of numbers,print 
+ the squrae of each value using forEach 
+ */
+let nums=[2,3,5,98];
+let calcSquare = (num)=>{
+    console.log( "Square is",num*num);
+};
+nums.forEach(calcSquare);
+/*
+Map creates a new array with the results of some operation
+the value its callback returns are used to form new array
+arr.map(callbackFnx)
+
+*/
+let numb =[98,78,67];
+let newArr= numb.map((val) => {
+return val*2
+});
+console.log(newArr);
